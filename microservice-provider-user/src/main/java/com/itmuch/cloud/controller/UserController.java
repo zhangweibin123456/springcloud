42b5e2f0430bbfe77351233608ba9b02af6d46b8
eacdy0000@126.com <eacdy0000@126.com>
2016-10-26 21:19:19 +0800

    整合feign

--- microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itmuch.cloud.entity.User;
@@ -39,5 +41,16 @@ public class UserController {
   public ServiceInstance showInfo() {
     ServiceInstance localServiceInstance = this.discoveryClient.getLocalServiceInstance();
     return localServiceInstance;
+  }
+
+  @PostMapping("/user")
+  public User postUser(@RequestBody User user) {
+    return user;
+  }
+
+  // 该请求不会成功
+  @GetMapping("/get-user")
+  public User getUser(User user) {
+    return user;
   }
 }
