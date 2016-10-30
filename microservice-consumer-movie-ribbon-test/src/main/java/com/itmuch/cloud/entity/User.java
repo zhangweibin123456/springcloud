119dbfb17506157531a1dd7ea67db16b9c1a1eab
eacdy0000@126.com <eacdy0000@126.com>
2016-10-30 17:31:19 +0800

    测试RestTemplate的坑

--- microservice-consumer-movie-ribbon-test/src/main/java/com/itmuch/cloud/entity/User.java
+++ microservice-consumer-movie-ribbon-test/src/main/java/com/itmuch/cloud/entity/User.java
@@ -2,39 +2,15 @@ package com.itmuch.cloud.entity;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class User {
-
-  public User(Long id, String username) {
-    super();
-    this.id = id;
-    this.username = username;
-  }
-
-  public User() {
-    super();
-  }
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  @Column
   private String username;
 
-  @Column
   private String name;
 
-  @Column
   private Short age;
 
-  @Column
   private BigDecimal balance;
 
   public Long getId() {
@@ -76,4 +52,5 @@ public class User {
   public void setBalance(BigDecimal balance) {
     this.balance = balance;
   }
+
 }
--- microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.itmuch.cloud.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.collect.Lists;
 import com.itmuch.cloud.entity.User;
 import com.itmuch.cloud.repository.UserRepository;
 import com.netflix.appinfo.InstanceInfo;
@@ -52,5 +56,18 @@ public class UserController {
   @GetMapping("/get-user")
   public User getUser(User user) {
     return user;
+  }
+
+  @GetMapping("list-all")
+  public List<User> listAll() {
+    ArrayList<User> list = Lists.newArrayList();
+    User user = new User(1L, "zhangsan");
+    User user2 = new User(2L, "zhangsan");
+    User user3 = new User(3L, "zhangsan");
+    list.add(user);
+    list.add(user2);
+    list.add(user3);
+    return list;
+
   }
 }
